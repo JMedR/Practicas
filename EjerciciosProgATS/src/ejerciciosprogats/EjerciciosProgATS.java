26754e666e736f6d17ffcdd8b17c4a9b2f63aee2
JMedR <u1803623@unimilitar.edu.co>
2020-02-14 22:39:19 -0500

    raiz cuadrada

--- EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
+++ EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejerciciosprogats;
-
-/**
- *
- * @author USER
- */
 public class EjerciciosProgATS {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+    double raiz = Math.sqrt(36.67);
+        System.out.print("La raiz cuadrada es: "+raiz);
+    
+    
+    
     }
     
 }
