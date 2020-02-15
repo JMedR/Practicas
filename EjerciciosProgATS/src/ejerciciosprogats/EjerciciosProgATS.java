d57b35462a4129d2f7bb18364c56fab3ae16c908
JMedR <u1803623@unimilitar.edu.co>
2020-02-15 16:04:59 -0500

    Ej1 de operadores

--- EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
+++ EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
@@ -1,11 +1,17 @@
+/*Suma de 3 notas e imprimir resultado*/
 package ejerciciosprogats;
 import java.util.Scanner;
 public class EjerciciosProgATS {
 
     public static void main(String[] args) {
-    double resultado=Math.random();
-    
-        System.out.println("El resultado es: "+resultado);
-    
+    float nota1,nota2,nota3,suma;
+    Scanner Entrada = new Scanner(System.in);
+        System.out.print("Digite las tres calificaciones: ");
+        nota1 = Entrada.nextFloat();
+        nota2 = Entrada.nextFloat();
+        nota3 = Entrada.nextFloat();//guardado de las notas
+        suma=nota1+nota2+nota3;
+        System.out.println("\nLa suma de las notas es: "+suma);
+           
     }  
 }
