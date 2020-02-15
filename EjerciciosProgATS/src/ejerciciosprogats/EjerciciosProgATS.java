388a7726e8285daeddd3b03d807983f5667555e4
JMedR <u1803623@unimilitar.edu.co>
2020-02-15 17:36:43 -0500

    If

--- EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
+++ EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
@@ -1,21 +1,18 @@
 /*1000 mensuales a trabajador, 150 comision por carro + 5% del valor de carro*/
 package ejerciciosprogats;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class EjerciciosProgATS {
     public static void main(String[] args) {
-    int Carros,b;
-    float Pago=1000,Vcarro;
-        System.out.print("Cuantos carros vendió en el mes? ");
-        Scanner Entrada = new Scanner(System.in);
-        Carros = Entrada.nextInt();
-        for(int a=0;a<Carros;a++){
-        b=a+1;
-        System.out.print("Ingrese el valor del carro "+b);
-        System.out.print(" : ");
-        Vcarro= Entrada.nextFloat();
-        Pago+=(Vcarro*0.05)+150;
-    }
-        System.out.print("\n\n\n\n\n\n\n\nEl ago final es de: "+Pago);
-        System.out.print("\n\n\n\n\n\n\n\n\n\n\n\n");
+        int numero, dato=5;
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Diguite un numero: "));
+        
+        if(numero!=dato){
+            JOptionPane.showMessageDialog(null, "EL numero es diferente de 5");
+        }
+        else{
+            JOptionPane.showMessageDialog(null,"EL numero es 5");
+        }
+ 
     }  
 }
