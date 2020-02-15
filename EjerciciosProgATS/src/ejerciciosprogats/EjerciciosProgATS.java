42523fc58307408a12bf60b9407e540eb32d4624
JMedR <u1803623@unimilitar.edu.co>
2020-02-15 15:48:26 -0500

    Redondeo de numero

--- EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
+++ EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
@@ -1,17 +1,12 @@
 package ejerciciosprogats;
-
 import java.util.Scanner;
-
 public class EjerciciosProgATS {
 
     public static void main(String[] args) {
-    double base,esponente;
-    Scanner EntradaDatos = new Scanner(System.in);
-    System.out.print("Ingrese el nuemro base para la potencia: ");
-    base = EntradaDatos.nextDouble();
-    System.out.print("\nIngrese el esponente: ");
-    esponente = EntradaDatos.nextDouble();
-    double resultado = Math.pow(base,esponente);
-    System.out.println("\n\nEl resultado es: " + resultado);
+    float Numero=4.4f;
+    int resultado=Math.round(Numero);
+    
+        System.out.println("El resultado es: "+resultado);
+    
     }  
 }
