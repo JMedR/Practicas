8ff3f6044e1bfbb68c8f4fec5fb99eca2738acb0
JMedR <u1803623@unimilitar.edu.co>
2020-02-15 15:43:31 -0500

    Potencia de base A y exponente B;

--- EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
+++ EjerciciosProgATS/src/ejerciciosprogats/EjerciciosProgATS.java
@@ -1,12 +1,17 @@
 package ejerciciosprogats;
+
+import java.util.Scanner;
+
 public class EjerciciosProgATS {
 
     public static void main(String[] args) {
-    double base=5,esponente=2;
+    double base,esponente;
+    Scanner EntradaDatos = new Scanner(System.in);
+    System.out.print("Ingrese el nuemro base para la potencia: ");
+    base = EntradaDatos.nextDouble();
+    System.out.print("\nIngrese el esponente: ");
+    esponente = EntradaDatos.nextDouble();
     double resultado = Math.pow(base,esponente);
-        System.out.println(resultado);
-    
-    
-    }
-    
+    System.out.println("\n\nEl resultado es: " + resultado);
+    }  
 }
